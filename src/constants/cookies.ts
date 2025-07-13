7ae761d05684fdad97c12f312848a0a619f81d5e
dowletgeldi99 <ballakow99@gmail.com>
2025-07-13 16:05:58 +0500

    update docker setup v7

--- src/constants/cookies.ts
+++ src/constants/cookies.ts
@@ -1,10 +1,12 @@
-import { CookieAttributes } from "js-cookie";
+import { CookieAttributes } from 'js-cookie'
 
-export const TOKEN_COOKIE_NAME = 'token';
-export const CHAT_TOKEN_COOKIE_NAME = 'chat_token';
+export const TOKEN_COOKIE_NAME = 'token'
+export const CHAT_TOKEN_COOKIE_NAME = 'chat_token'
 export const COOKIE_OPTIONS: CookieAttributes = {
-    expires: 7, // 7 days
-    path: '/',
-    sameSite: 'strict',
-    secure: process.env.NODE_ENV === 'production' // Secure in production
-};
+  expires: 7, // 7 days
+  path: '/',
+  sameSite: 'strict',
+  //TODO: update on ssl setup
+  // secure: process.env.NODE_ENV === 'production' // Secure in production
+  secure: false, // Secure in production
+}
